dfd203ea7a037028f1d36e89930e4bc301f71a40
kashif <kashif@greytip.com>
2019-02-05 16:44:49 +0530

    implement some datastructures

---
+++ src/com/kashif/datastructures/tree/BinaryTreeTraversals1.java
@@ -0,0 +1,23 @@
+package com.kashif.datastructures.tree;
+
+public class BinaryTreeTraversals1 {
+
+	class Node {
+		int data;
+		Node left, right;
+		
+		Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	
+	class BinaryTree {
+		Node root;
+		
+		BinaryTree(Node root) {
+			
+		}
+	}
+}
